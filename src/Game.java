9dafb1a99acd1d59fdbc9e87c82ac55193d55ac2
Martin Švanda <120254528+EmperorKunDis@users.noreply.github.com>
2023-09-10 08:08:53 +0200

    OOP

---
+++ src/Game.java
@@ -0,0 +1,21 @@
+public class Game {
+
+    String make = "FromSoftware";
+    String titel;
+    {
+        titel = "Elden Ring";
+    }
+
+    int year = 2022;
+    String played = "Complete";
+    double price = 69.99;
+    boolean Platina = false;
+
+    void playin() {
+        System.out.println("I'm tryin to mastering platinum trophy.");
+    }
+    void invadin() {
+        System.out.println("I love to kill them :).");
+    }
+
+}
--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,9 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        Game myGame = new Game();
+        System.out.println(myGame.titel);
+
+        myGame.playin();
     }
 }
