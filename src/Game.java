9d1eb3d5d47c12ec86c803499c3ae95054834961
Martin Švanda <120254528+EmperorKunDis@users.noreply.github.com>
2023-09-10 22:05:57 +0200

    Game list
    Finally Its thats how I want it

--- src/Game.java
+++ src/Game.java
@@ -1,39 +1,92 @@
 public class Game {
 
     String make;
-     String title;
-     double year;
-     String played;
+    String title;
+    int year;
+    boolean haveit;
     String price;
-   boolean Platina;
-    Game(String make, String title, double year, String played, String price, boolean Platina) {
+    boolean Platina;
+    boolean PC;
+    boolean wanna;
+    String SPlatina;
+    String SPC;
+    String Swanna;
+    String Shaveit;
 
+    Game(String make, String title, int year, boolean haveit, String price, boolean Platina, boolean PC, boolean wanna)
+    {
+        this.make = make;
+        this.title = title;
+        this.year = year;
+        this.haveit = haveit;
+        this.price = price;
+        this.Platina = Platina;
+        this.PC = PC;
+        this.wanna = wanna;
 
-        Game.make = make;
-        Game.title = title;
-        Game.year = year;
-        Game.played = played;
-        Game.price = price;
-        Game.Platina = Platina;
-    }
+        if (haveit == true) {
+            Shaveit = "| I have it. |";
+            Swanna = " ";
+            if (PC == true) {
+                SPC = "| It on PC. |";
+                if (Platina == true) {
+                    SPlatina = "| I have it. |      ";
+                } else {
+                    SPlatina = "| Im mastering it. |";
+                }
+            } else {
+                SPC = "| I dont have how to play it. |";
+                if (wanna == true) {
+                    SPlatina = " ";
+                    Swanna = " ";
+                } else {
+                    SPlatina = "| Until it is not on PC I cant do it. |";
+                    }
+            }
 
-
-    void playin() {
-        if (Platina=true) {
-            System.out.println("I'm tryin to mastering platinum trophy.");
-        }  else{
-            System.out.println("I'm gona DYING TY. "+title+" is not my strong side.");
-        }
-    }
-
-    void invadin() {
-        if (Platina=true) {
-            System.out.println("I love to kill them :).");
         } else {
-            System.out.println("OOOOuuuuuu ruuuuuuun, bitch ruuuuuuuuuu.");
+            Shaveit ="| I dont have it |";
+            if (PC == true) {
+                Swanna = "| No i dont like everythin from Hidetaka Miayzaki xD xD xD Just kidding. Of Course I wanna it. |"
+                        +"| I need it. I cant breath without it. |";
+                RunToBuyIt();
+            } else {
+                SPC = "| No Its only on fucking playstation. |";
+                SPlatina = "| Until it is not on PC I cant do it. |";
+                Swanna = "| I wish it more than world peace. |";
 
-            System.out.println("I love to kill them :).");
+            }
         }
+    }
+    void stat() {
+        System.out.println(make+title+year+Shaveit+price+SPlatina+SPC+Swanna);
+        }
+    void playin() {
+        if (PC = true) {
+            if (haveit == true) {
+                if (Platina = true) {
+                            System.out.println("| Just Invadin i dont need play it normal anymore. |");
+                } else {
+                            System.out.println("| I'm tryin to mastering platinum trophy. |");}
+            } else {
+                        System.out.println("| I wish, I have it. |");}
+        } else {
+                    System.out.println("| I wish, I can. |");}
+    }
+    void invadin() {
+        if (PC=true) {
+            if (haveit == true) {
+                if (Platina = true) {
+                            System.out.println("| I love to kill them :). |");
+                } else {
+                            System.out.println("| OOOOuuuuuu ruuuuuuun, bitch ruuuuuuuuuun. |");}
+            } else {
+                        System.out.println("| I wish, I have it. |");}
+        } else {
+                    System.out.println("| I think I need mastered it for first time and then someone Invadet. |");}
+    }
+    void RunToBuyIt() {
+        System.out.println("| I'm on my way to buy it. |");
     }
 }
 
--- src/Main.java
+++ src/Main.java
@@ -1,23 +1,41 @@
 public class Main {
     public static void main(String[] args) {
 
-        Game myGame0 = new Game("FromSoftware - Hidetaka Miayzaki", "Elden Ring", 2022,
-                "Complete", "SixtyNine", true);
-        Game myGame1 = new Game("FromSoftware - Hidetaka Miayzaki", "Sekiro", 2019, "Yes",
-                "Same", false);
-        Game myGame2 = new Game("FromSoftware - Hidetaka Miayzaki", "Darksouls III", 2016,
-                "Yes", "Me too", false);
-        Game myGame3 = new Game("FromSoftware - Hidetaka Miayzaki", "Demonsouls", 2011,
-                "Yes", "Just like that", false);
-        Game myGame4 = new Game("FromSoftware - Hidetaka Miayzaki", "Bloodborne", 2015,
-                "Yes", "Same", false);
+        Game myGame0 = new Game("FromSoftware - Hidetaka Miayzaki.    "+"\n", "                     " +
+                " Elden Ring     -", 2022    , true, "    | 69,99- |", true, true,
+                true);
+        Game myGame1 = new Game("FromSoftware - Hidetaka Miayzaki.    "+"\n", "                     " +
+                " Sekiro         -", 2019     , true, "    | 69,99- |", false, true,
+                true);
+        Game myGame2 = new Game("FromSoftware - Hidetaka Miayzaki.    "+"\n", "                     " +
+                " Darksouls III  -", 2016    , true, "    | 69,99- |", false, true,
+                true);
+        Game myGame3 = new Game("FromSoftware - Hidetaka Miayzaki.    "+"\n", "                     " +
+                " Demonsouls     -", 2011    , false, "| 69,99- |", false, false,
+                true);
+        Game myGame4 = new Game("FromSoftware - Hidetaka Miayzaki.    "+"\n", "                     " +
+                " Bloodborne     -", 2015    , false, "| 69,99- |", false, false,
+                true);
 
-        System.out.println(myGame0.make);
-        System.out.println(myGame1.title);
-        System.out.println(myGame2.year);
-        System.out.println(myGame3.played);
-        System.out.println(myGame4.title);
+        System.out.println(myGame0.make+""+myGame0.title+""+myGame0.year+""+myGame0.Shaveit+""+myGame0.price+"" +
+                            ""+myGame0.SPlatina+""+myGame0.SPC+""+myGame0.Swanna);
+        //this is first try : System.out.println(myGame1.make+" "+myGame1.title+" "+myGame1.year+" "+myGame1.haveit+" "+myGame1.price+" " +
+                //" "+myGame1.SPlatina+" "+myGame1.SPC+" "+myGame1.Swanna);
+        //System.out.println(myGame2.make+" "+myGame2.title+" "+myGame2.year+" "+myGame2.haveit+" "+myGame2.price+" " +
+                //" "+myGame2.SPlatina+" "+myGame2.SPC+" "+myGame2.Swanna);
+        //System.out.println(myGame3.make+" "+myGame3.title+" "+myGame3.year+" "+myGame3.haveit+" "+myGame3.price+" " +
+                //" "+myGame3.SPlatina+" "+myGame3.SPC+" "+myGame3.Swanna);
+        //System.out.println(myGame4.make+" "+myGame4.title+" "+myGame4.year+" "+myGame4.haveit+" "+myGame4.price+" " +
+                //" "+myGame4.SPlatina+" "+myGame4.SPC+" "+myGame4.Swanna);
+
+        myGame0.stat();
+        myGame1.stat();
+        myGame2.stat();
+        myGame3.stat();
+        myGame4.stat();
 
         myGame1.playin();
+        myGame2.invadin();
+        myGame2.RunToBuyIt();
     }
 }
