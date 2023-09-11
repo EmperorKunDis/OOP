35173b01668bd9dd4cffc197bfa80e91655d6edb
Martin Švanda <120254528+EmperorKunDis@users.noreply.github.com>
2023-09-11 22:22:18 +0200

    TwentyDiceRoller
    This is twenty dice roller with Global variables with variables writen outside a method

---
+++ src/TwentyRoller.java
@@ -0,0 +1,17 @@
+import java.util.Random;
+
+public class TwentyRoller {
+    Random random;
+    int number;
+
+    TwentyRoller() {
+        random = new Random();
+        roll();
+    }
+
+    void roll() {
+        number = random.nextInt(20)+1;
+        System.out.println(number);
+    }
+
+}
